0b92991ee1afb589b95cee99c7404b07ff50efd0
DMoises <3033997670108@ingenieria.usac.edu.gt>
2025-11-26 19:08:19 -0600

    fix(home): arreglar error visual de botón cerrar sesión

--- feynman-processor/src/app/home/home.ts
+++ feynman-processor/src/app/home/home.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SupabaseService } from '../services/supabase';
 import { TreeViewer } from '../tree-viewer/tree-viewer';
 import { DocEditor } from '../doc-editor/doc-editor';
+import { AuthService } from '../services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, TreeViewer, DocEditor],
+  imports: [CommonModule, TreeViewer, DocEditor],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -15,7 +16,7 @@ export class Home implements OnInit{
   // 1. Variable para controlar el menú lateral (sidebar)
   isSidebarOpen = true;
   protected readonly title = signal('feynman-processor');
-  constructor(private supabaseService: SupabaseService) {}
+  constructor(private supabaseService: SupabaseService, public authService: AuthService) {}
   ngOnInit(): void {
     console.log('App initialized');
     this.testSupabaseConnection();
@@ -36,5 +37,8 @@ export class Home implements OnInit{
   }
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+  async logout(){
+    await this.authService.signOut();
   }
 }
